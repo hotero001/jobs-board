7f645efd91614b97b66a9b60691c6c9dd1bf9925
Hector Otero <otero111@hotmail.com>
2017-02-06 01:42:41 -0600

    added more controller tests

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -8,4 +8,25 @@ describe JobsController do
       expect(response).to render_template("index")
     end
   end
+
+  describe "GET engineering_jobs" do
+    it "renders the engineering jobs template" do
+      get :engineering_jobs
+      expect(response).to render_template("engineering_jobs")
+    end
+  end
+
+  describe "GET call_center_jobs" do
+    it "renders the call_center_jobs template" do
+      get :call_center_jobs
+      expect(response).to render_template("call_center_jobs")
+    end
+  end
+
+  describe "GET education_jobs" do
+    it "renders the education_jobs template" do
+      get :education_jobs
+      expect(response).to render_template("education_jobs")
+    end
+  end
 end
