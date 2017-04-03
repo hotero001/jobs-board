25afbd46276ee872eaf66dcc1e39854f48badc7c
Hector Otero <otero111@hotmail.com>
2017-04-02 22:20:58 -0500

    added emailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      ExampleMailer.sample_email(@user).deliver
       redirect_to '/'
     else
       render :new
